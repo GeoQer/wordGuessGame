4883bbb2639880c59913492a4c5b2c27ddd17e15
GeoQer <40548603+GeoQer@users.noreply.github.com>
2018-08-14 18:30:52 -0700

    psuedo code frame

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//array with word arrays
+//random word selection
+//retrieve length of selected word array
+//show "_" for each letter in the word
+//until all letters are matched || usedLetters arrayLength == 7 {event.key look for letter input from user}
+//compare letters in array to user input to determine a match
+//if match then change "_" to the matching letter
+//else compare input to letters in usedLetters array
+//if no matches add non-matching letter to "used letters" array
+//else wait for new input
+//if all letters match display "win" - Display button "Play Again"
+//if usedLetter arrayLength == 7 display "lose" - Display button "Try Again"
+//
